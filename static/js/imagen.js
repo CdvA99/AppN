31fed0bfd29729baea2dcca0f3ea3b27c42d368b
CdvA99 <cdva1799@gmail.com>
2024-01-23 18:42:47 -0500

    Proyecto

---
+++ static/js/imagen.js
@@ -0,0 +1,12 @@
+$('input[name="foto"]').change(function () {
+    var input = this;
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imagen-preview').attr('src', e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+});
